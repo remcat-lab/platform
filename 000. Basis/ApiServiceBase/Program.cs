fab1121c55539755cf8a04263542ff96a7e8304a
remcat-lab <louvetemp@gmail.com>
2025-06-02 07:57:59 +0900

    Update Program.cs

--- 000. Basis/ApiServiceBase/Program.cs
+++ 000. Basis/ApiServiceBase/Program.cs
@@ -1,9 +1,6 @@
-using ApiHost;
+using ApiServer;
+using System.Reflection;
 
-var port = 5000;
-var portArg = args.FirstOrDefault(a => a.StartsWith("--port="));
-if (portArg != null && int.TryParse(portArg.Split('=')[1], out var parsed))
-    port = parsed;
-
-var app = ApiServerBuilder.Build(args, port);
-app.Run();
+ApiServerBuilder.Run(args,
+    Assembly.Load("CredentialService"),
+    Assembly.Load("ServiceA"));
