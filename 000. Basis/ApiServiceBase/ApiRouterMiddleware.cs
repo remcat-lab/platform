4b3602c16c122ccba256ce20bd0e16d91c57066a
remcat-lab <louvetemp@gmail.com>
2025-06-02 07:53:09 +0900

    Update ApiRouterMiddleware.cs

--- 000. Basis/ApiServiceBase/ApiRouterMiddleware.cs
+++ 000. Basis/ApiServiceBase/ApiRouterMiddleware.cs
@@ -2,31 +2,24 @@ public class ApiRouterMiddleware
 {
     private readonly RequestDelegate _next;
     private readonly Dictionary<string, Type> _apiMap;
-    private readonly IServiceProvider _provider;
+    private readonly IServiceProvider _serviceProvider;
 
-    public ApiRouterMiddleware(RequestDelegate next,
-                               Dictionary<string, Type> apiMap,
-                               IServiceProvider provider)
+    public ApiRouterMiddleware(RequestDelegate next, Dictionary<string, Type> apiMap, IServiceProvider serviceProvider)
     {
         _next = next;
         _apiMap = apiMap;
-        _provider = provider;
+        _serviceProvider = serviceProvider;
     }
 
     public async Task InvokeAsync(HttpContext context)
     {
-        var path = context.Request.Path.Value?.ToLowerInvariant();
+        var path = context.Request.Path.ToString().ToLowerInvariant();
 
-        if (path != null && _apiMap.TryGetValue(path, out var handlerType))
+        if (_apiMap.TryGetValue(path, out var handlerType))
         {
-            if (_provider.GetService(handlerType) is IApiHandler handler)
-            {
-                await handler.HandleAsync(context);
-                return;
-            }
-
-            context.Response.StatusCode = 500;
-            await context.Response.WriteAsync("Handler type not resolved");
+            using var scope = _serviceProvider.CreateScope();
+            var handler = (IApiHandler)scope.ServiceProvider.GetRequiredService(handlerType);
+            await handler.HandleAsync(context);
             return;
         }
 
