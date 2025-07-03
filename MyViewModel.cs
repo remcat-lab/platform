268c1717bb00a1c3107f5aff8fd0c3bf0c1cea6f
remcat-lab <louvetemp@gmail.com>
2025-07-03 20:59:38 +0900

    Create MyViewModel.cs

---
+++ MyViewModel.cs
@@ -0,0 +1,26 @@
+public partial class MyViewModel : ObservableObject
+{
+    [ObservableProperty]
+    private bool isBusy;
+
+    [RelayCommand]
+    private async Task SaveAsync()
+    {
+        IsBusy = true;
+        try
+        {
+            await Task.Delay(2000);
+        }
+        finally
+        {
+            IsBusy = false;
+        }
+    }
+
+    private bool CanSave() => !IsBusy;
+
+    partial void OnIsBusyChanged(bool oldValue, bool newValue)
+    {
+        SaveCommand.NotifyCanExecuteChanged();
+    }
+}
